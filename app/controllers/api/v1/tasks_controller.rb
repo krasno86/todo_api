dc228699e3101619f503e2dc13fff2882e97eeae
krasno86 <krasno86@mail.ru>
2018-09-26 17:17:22 +0300

    remove serialize method to concern, fix projects/index tests

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -3,6 +3,7 @@
 module Api::V1
   class TasksController < ApplicationController
     include DeviseTokenAuth::Concerns::SetUserByToken
+    include Serialize_object
 
     before_action :authenticate_user!
     before_action :set_task, only: [:show, :update]
@@ -10,18 +11,17 @@ module Api::V1
 
     def index
       @tasks = @project.tasks.all.order("created_at DESC")
-      render json: @tasks, status: 200
+      render json: serialized_object(@tasks), status: 200
     end
 
     def show
-      render json: @task, status: 200
+      render json: serialized_object(@task), status: 200
     end
 
     def create
       @task = @project.tasks.new(task_params)
       if @task.save!
-        render json: @task, status: 201
-        # render json: ProjectSerializer.new(@project).serialized_json, status: 201
+        render json: serialized_object(@task), status: 201
       else
         render json: @task.errors.messages, status: 422
       end
@@ -29,12 +29,10 @@ module Api::V1
 
     def update
       @task.update(task_params)
-      render json: @task
+      render json: serialized_object(@task)
     end
 
     def destroy
-      p params
-      p current_user
       if Task.destroy(params[:id])
         head :no_content, status: :ok
       else
